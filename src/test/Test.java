ea426ff622b2b2ddcb41b1931dc9218b8a7b6918
vijay <vijay@localhost.localdomain>
2013-04-19 01:13:18 -0700

    Clean up and comments

--- src/test/Test.java
+++ src/test/Test.java
@@ -15,16 +15,15 @@ public class Test {
     /**
      * @param args the command line arguments
      */
-    public static void printAList(ArrayList l){
+    public static void printAList(ArrayList<Integer> l){
         System.out.print("Array = ");
-        for (Iterator it = l.iterator(); it.hasNext();) {
-            int i = (int) it.next();
+        for (Integer i : l) {
             System.out.print(i+" ");
         }
         System.out.print("\n");
     }
-    public static ArrayList Merge(ArrayList l1, ArrayList l2) {
-        ArrayList ret = new ArrayList();
+    public static ArrayList<Integer> Merge(ArrayList<Integer> l1, ArrayList<Integer> l2) {
+        ArrayList<Integer> ret = new ArrayList<Integer>();
         int len = l1.size()+l2.size();
         int pos1 = 0;
         int pos2 = 0;
@@ -34,9 +33,9 @@ public class Test {
         Integer k = new Integer(0);
         for (int i=0; i<len; i++){
             if (!l1end)
-                j = (Integer) l1.get(pos1);
+                j = l1.get(pos1);
             if (!l2end)
-                k = (Integer) l2.get(pos2);
+                k = l2.get(pos2);
             
             if (l1end) {
                 ret.add(k);
@@ -60,22 +59,21 @@ public class Test {
                     l2end = true;
             }
         }
-        
-        
+              
         return ret;
     }
-    public static ArrayList MergeSplit(ArrayList l, int start, int end){
-        System.out.println("start = "+start+" end = "+end);
-        ArrayList ret;
-        if (end == start) {
-            ret = new ArrayList();
+    public static ArrayList<Integer> MergeSplit(ArrayList<Integer> l, int start, int end){
+        //System.out.println("start = "+start+" end = "+end);
+        ArrayList<Integer> ret;
+        if (end == start) { //Termination case 1 (only one element)
+            ret = new ArrayList<Integer>();
             ret.add(l.get(start));
             return ret;
         }
-        if ((end-start)==1) {
-            Integer i = (Integer) l.get(start);
-            Integer j = (Integer) l.get(end);
-            ret = new ArrayList();
+        if ((end-start)==1) { // Termination case 2 (two elements)
+            Integer i = l.get(start);
+            Integer j = l.get(end);
+            ret = new ArrayList<Integer>();
             if (i.intValue() <= j.intValue()) {
                 ret.add(i);
                 ret.add(j);
@@ -86,21 +84,23 @@ public class Test {
             }
             return ret;
         }
-        ArrayList l1 = MergeSplit(l,start, (end-start)/2+start);        
-        ArrayList l2 = MergeSplit(l, ((end-start)/2)+1+start, end);
-        
+        // First half of array
+        ArrayList<Integer> l1 = MergeSplit(l,start, (end-start)/2+start); 
+        // Second half of array
+        ArrayList<Integer> l2 = MergeSplit(l, ((end-start)/2)+1+start, end);
+        // Merge sorted 1st and 2nd half
         ret = Merge(l1,l2);
-        printAList(ret);
         return ret;
     }
     
-    public static ArrayList MergeSort(ArrayList l) {
+    public static ArrayList<Integer> MergeSort(ArrayList<Integer> l) {
         int len = l.size();
         if (len <= 1) 
             return l;
-        ArrayList l1 = MergeSplit(l,0,(len-1)/2);
-        ArrayList l2 = MergeSplit(l,((len-1)/2)+1, len-1);
-        ArrayList ret = Merge(l1,l2);
+        //Split array into 2
+        ArrayList<Integer> l1 = MergeSplit(l,0,(len-1)/2); 
+        ArrayList<Integer> l2 = MergeSplit(l,((len-1)/2)+1, len-1);
+        ArrayList<Integer> ret = Merge(l1,l2); // Merge results
         printAList(ret);
         return ret;
     }
@@ -108,7 +108,7 @@ public class Test {
    
     public static void main(String[] args) {
    
-       ArrayList MS = new ArrayList();
+       ArrayList<Integer> MS = new ArrayList<Integer>();
        
        if (args.length > 0) {
            System.out.println("arglen = "+args.length);
@@ -128,6 +128,6 @@ public class Test {
        }
        
        printAList(MS);
-       ArrayList l = MergeSort(MS); 
+       ArrayList<Integer> l = MergeSort(MS); 
     }
 }
